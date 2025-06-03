2977517f64ae433ba2364b9c0af97cb58159492a
Jeremy Fleischman <jeremyfleischman@gmail.com>
2025-06-03 11:28:58 -0700

    Fix assertion to account for `APP_KEY_FILE`
    Freescout does support the `APP_KEY_FILE` setting, but if I try to use
    it instead of `APP_KEY._secret`, I run afoul of [this
    assertion](https://cyberchaos.dev/e1mo/freescout-nix-flake/-/blob/2b5bcee06a673c13c5b5a62b4f4dd1300ce85903/module.nix#L253):
    
    ```nix
        assertions = [{
          assertion = (app_config ? "APP_KEY");
          message = "`services.freescout.settings.APP_KEY` is required!";
        }];
    ```

--- module.nix
+++ module.nix
@@ -249,7 +249,7 @@ in {
 
   config = mkIf cfg.enable {
     assertions = [{
-      assertion = (app_config ? "APP_KEY");
+      assertion = (app_config ? "APP_KEY" || app_config ? "APP_KEY_FILE");
       message = "`services.freescout.settings.APP_KEY` is required!";
     }];
 
